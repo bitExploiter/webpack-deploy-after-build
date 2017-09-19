957f36d016cff4abf8596025ad58591f42d02a8f
Karel Mácha <k.macha@clevershuttle.de>
2017-09-19 10:58:16 +0200

    Implements an option for clearing the destination directory before deploying new content.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
-var ncp = require('ncp').ncp;
+const ncp = require('ncp').ncp;
+const del = require('del');
 
 function WebPackDeployAfterBuild(options) {
     var defaultOptions = {
-
+        clearDestDir: false,
     };
 
     this.options = Object.assign(defaultOptions, options);
@@ -14,26 +15,32 @@ WebPackDeployAfterBuild.prototype.apply = function(compiler) {
 
     compiler.plugin("done", (stats) => {
         console.log(Object.keys(stats.compilation.assets));
-    console.log("\nExecuting Deploy on done...");
-    ncp(options.from, options.to, function(err) {
-        if (err) {
-            console.error("Err in ncp check the paths");
+        console.log("\nExecuting Deploy on done...");
+        if (options.clearDestDir) {
+            del.sync([`${options.to}/*`], {force: true});
         }
-        console.log(`Finished deploying ${options.from} to ${options.to}`);
+        ncp(options.from, options.to, function(err) {
+            if (err) {
+                console.error("Err in ncp check the paths");
+            }
+            console.log(`Finished deploying ${options.from} to ${options.to}`);
+        });
     });
-});
 
     compiler.plugin("after-emit", (compilation, callback) => {
         console.log(Object.keys(compilation.assets));
-    console.log("\nExecuting Deploy on after-emit...");
-    ncp(options.from, options.to, function(err) {
-        if (err) {
-            console.error("Err in ncp");
+        console.log("\nExecuting Deploy on after-emit...");
+        if (options.clearDestDir) {
+            del.sync([`${options.to}/*`], {force: true});
         }
-        console.log(`Finished deploying ${options.from} to ${options.to}`);
-        callback();
+        ncp(options.from, options.to, function(err) {
+            if (err) {
+                console.error("Err in ncp");
+            }
+            console.log(`Finished deploying ${options.from} to ${options.to}`);
+            callback();
+        });
     });
-});
 };
 
 module.exports = WebPackDeployAfterBuild;
